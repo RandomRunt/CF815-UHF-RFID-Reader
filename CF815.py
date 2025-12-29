fdaa9d6f5006880f81cb870fae2729682eaeadc8
James Lu <52562639+RandomRunt@users.noreply.github.com>
2025-12-29 13:14:52 +1100

    [TEST] Added antenna RF Modulation profile test 4

--- CF815.py
+++ CF815.py
@@ -372,20 +372,19 @@ class RFIDReaderTCP:
             scan_time: (optional) Scan time in scan_time*100ms
         """
         
-        print(f"\n--- ROBUST SCAN (Profile 1: Miller-4) ---")
+        # print(f"\n--- ROBUST SCAN (Profile 1: Miller-4) ---")
+        # # 1. FORCE PROFILE 1 (Miller-4 250KHz)
+        # # Command 0x7F: Setup Reader Profile
+        # # Data: 0x81 (Bit7=1 for 'Modify', Value=1 for 'Profile 1')
+        # print("Configuring Reader to Profile 1 (Miller-4)...")
+        # self.send_command(0x7F, data=[0x81], address=address)
+        # resp = self.receive_response()
+        # if resp[2] == 0x7F and resp[3] == 0x00:
+        #     print(" -> Profile set successfully.")
+        # else:
+        #     print(" -> Failed to set profile (or already set).")
         
-        # 1. FORCE PROFILE 1 (Miller-4 250KHz)
-        # Command 0x7F: Setup Reader Profile
-        # Data: 0x81 (Bit7=1 for 'Modify', Value=1 for 'Profile 1')
-        print("Configuring Reader to Profile 1 (Miller-4)...")
-        self.send_command(0x7F, data=[0x81], address=address)
-        resp = self.receive_response()
-        if resp[2] == 0x7F and resp[3] == 0x00:
-            print(" -> Profile set successfully.")
-        else:
-            print(" -> Failed to set profile (or already set).")
-        
-        print("\nPerforming Inventory Scan (Press Ctrl+C to stop)...")
+        # print("\nPerforming Inventory Scan (Press Ctrl+C to stop)...")
         
         # 1. Note start time
         start_time = time.time()
@@ -407,7 +406,7 @@ class RFIDReaderTCP:
         if ant is not None:
             data.append(ant)
         if scan_time is not None:
-            data.append(scan_time)
+            data.append(0x05)
 
         while time.time() - start_time < scan_time_sec:  # Limit total inventory time to 30 seconds
             # 2. Send inventory command

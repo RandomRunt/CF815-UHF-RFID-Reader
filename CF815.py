ada7f1b957b9572f1a547145930ae7c5c8f4278f
James Lu <52562639+RandomRunt@users.noreply.github.com>
2025-12-29 11:03:37 +1100

    [FIX] attempting to try s0 config

--- CF815.py
+++ CF815.py
@@ -650,7 +650,7 @@ if __name__ == "__main__":
                     reader.inventory(
                         address=READER_ADDRESS,
                         q_value=0b00000110,   # 0x06 = 0b00000110 (No Stats, Standard Strategy, No FastID, No Phase Info, Q=6)
-                        session=0xFF,  # Smart session
+                        session=0x00,  # Smart session
                         mask_mem=0x01,
                         mask_adr=0x0000,
                         mask_len=0x00,
